bd85c2d195e42fad6b3d8247961e43d4092c267e
PizzaCrust <pizzacrustcoder@gmail.com>
2021-01-22 21:42:31 +0700

    ..

--- build.rs
+++ build.rs
@@ -4,18 +4,18 @@ use std::fs::DirEntry;
 use diffy::{Patch, apply, ApplyError};
 
 fn main() -> anyhow::Result<()> {
-    if Path::new("ooz/kraken_patched.cpp").exists() {
-        return Ok(())
+    println!("cargo:rerun-if-changed=build.rs");
+    if !Path::new("ooz/kraken_patched.cpp").exists() {
+        if !Path::new("ooz/kraken.cpp").exists() {
+            panic!("Kraken.cpp doesn't exist. Please fetch git submodules.")
+        }
+        let patch = std::fs::read_to_string("kraken.patch")?;
+        let patch = Patch::from_str(&patch)?;
+        let text = std::fs::read_to_string("ooz/kraken.cpp")?;
+        let text = apply(&text, &patch)?;
+        std::fs::write("ooz/kraken_patched.cpp", text)?;
+        std::fs::remove_file("ooz/kraken.cpp")?;
     }
-    if !Path::new("ooz/kraken.cpp").exists() {
-        panic!("Kraken.cpp doesn't exist. Please fetch git submodules.")
-    }
-    let patch = std::fs::read_to_string("kraken.patch")?;
-    let patch = Patch::from_str(&patch)?;
-    let text = std::fs::read_to_string("ooz/kraken.cpp")?;
-    let text = apply(&text, &patch)?;
-    std::fs::write("ooz/kraken_patched.cpp", text)?;
-    std::fs::remove_file("ooz/kraken.cpp")?;
     Build::new()
         .cpp(true)
         .files(std::fs::read_dir("ooz")?.filter_map(|x| match x {
