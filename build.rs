668a85072fc4df3058fcc125f8eaf7beb212054a
PizzaCrust <pizzacrustcoder@gmail.com>
2021-02-03 00:00:24 +0700

    fmt

--- build.rs
+++ build.rs
@@ -5,10 +5,20 @@ fn main() -> anyhow::Result<()> {
     let mut builder = Build::new();
     builder
         .cpp(true)
-        .files(std::fs::read_dir("ooz")?.filter_map(|x| match x {
-            Ok(x) => if x.file_name().to_str()?.ends_with(".cpp") { Some(x) } else { None },
-            Err(_) => None
-        }).map(|x| x.path()))
+        .files(
+            std::fs::read_dir("ooz")?
+                .filter_map(|x| match x {
+                    Ok(x) => {
+                        if x.file_name().to_str()?.ends_with(".cpp") {
+                            Some(x)
+                        } else {
+                            None
+                        }
+                    }
+                    Err(_) => None,
+                })
+                .map(|x| x.path()),
+        )
         .compile("ooz");
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub struct CompressOptions {
     make_qhcrc: i32,
     max_local_dictionary_size: i32,
     make_long_range_matcher: i32,
-    hash_bits: i32
+    hash_bits: i32,
 }
 
 #[repr(i32)]
@@ -21,7 +21,7 @@ pub enum Compressor {
     Kraken = 8,
     Mermaid = 9,
     Selkie = 11,
-    Leviathan = 13
+    Leviathan = 13,
 }
 
 #[repr(i32)]
@@ -35,17 +35,12 @@ pub enum CompressorLevel {
     Optimal2,
     Optimal3,
     Optimal4,
-    Optimal5
+    Optimal5,
 }
 
 #[link(name = "ooz")]
 extern "C" {
-    pub fn Kraken_Decompress(
-        src: *const u8,
-        src_len: usize,
-        dst: *mut u8,
-        dst_len: usize
-    ) -> i32;
+    pub fn Kraken_Decompress(src: *const u8, src_len: usize, dst: *mut u8, dst_len: usize) -> i32;
     pub fn Compress(
         codec_id: Compressor,
         src_in: *const u8,
@@ -54,20 +49,21 @@ extern "C" {
         level: CompressorLevel,
         compress_options: *const CompressOptions,
         src_window_base: *const u8,
-        lrm: *const c_void
+        lrm: *const c_void,
     ) -> i32;
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{Kraken_Decompress, Compress, Compressor, CompressorLevel};
+    use crate::{Compress, Compressor, CompressorLevel, Kraken_Decompress};
     use std::ptr;
 
     #[test]
     fn unsafe_decompress() {
         let src = include_bytes!("../mermaid-fortnite.dump");
         let mut dst = vec![0u8; 405273];
-        let result = unsafe { Kraken_Decompress(src.as_ptr(), src.len(), dst.as_mut_ptr(), dst.len()) };
+        let result =
+            unsafe { Kraken_Decompress(src.as_ptr(), src.len(), dst.as_mut_ptr(), dst.len()) };
         assert_eq!(result as usize, dst.len());
     }
 
@@ -84,7 +80,7 @@ mod tests {
                 CompressorLevel::Optimal1,
                 ptr::null(),
                 ptr::null(),
-                ptr::null()
+                ptr::null(),
             )
         };
         assert!(result > 0);
